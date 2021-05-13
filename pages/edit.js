76e8c14bf1cdf26e54806c843533f7550d36a775
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-05-13 04:41:17 -0400

    add styling & remove not working code from Home

--- pages/edit.js
+++ pages/edit.js
@@ -1,26 +1,14 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Edit.module.css';
 
-// Create Graph to hold data
-import { Graph, Node } from '../utils/graph';
-import { generateRandomNetwork } from '../utils/helpers';
+export default function Home({ userGraph = [], setUserGraph, mockData }) {
+  console.log(mockData)
 
-export default function Home() {
-  const [graph, setGraph] = useState(new Graph())
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
-        const mockData = generateRandomNetwork(res, graph, 150);
-        setGraph(mockData)
-      })
-  })
   return (
     <div className={styles.container}>
       <Head>
-        <title>Social Graph - Edit</title>
+        <title>Social Graph - Home</title>
         <meta name="description" content="Social network graph to display contact groups. Edit the content displayed by the graph." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- pages/view.js
+++ pages/view.js
@@ -1,25 +1,12 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/View.module.css'
 
-// Create Graph to hold data
-import { Graph, Node } from '../utils/graph';
-import { generateRandomNetwork } from '../utils/helpers';
-
-export default function UserPage({ userGraph=[], setUserGraph }) {
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
-        const mockData = generateRandomNetwork(res, graph, 150);
-        setGraph(mockData)
-      })
-  })
+export default function Home({ userGraph = [], setUserGraph }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Social Graph - View</title>
+        <title>Social Graph - Home</title>
         <meta name="description" content="Social network graph to display contact groups. View network graph here." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
