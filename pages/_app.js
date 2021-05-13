1325b1fe19b57c59243c7cbe2332337f8ddbbeb8
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-05-13 04:17:06 -0400

    display DefaultLayout in Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from 'react'
 import '../styles/globals.css'
+import DefaultLayout from '../components/_layouts/DefaultLayout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [userList, setUserList] = useState([]);
+
+  const getLayout = Component.getLayout || (page => <DefaultLayout children={page} userList={userList} setUserList={setUserList} />)
+
+  return getLayout(
+    <Component {...pageProps} userList={userList} setUserList={setUserList} />
+  )
 }
 
-export default MyApp
+export default MyApp;
