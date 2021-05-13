0f412b55db258416215593a9f45be24c8a38d23a
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-05-13 05:22:08 -0400

    remove unused parts

--- pages/edit.js
+++ pages/edit.js
@@ -1,14 +1,11 @@
 import Head from 'next/head'
-import { useState, useEffect } from 'react';
 import styles from '../styles/Edit.module.css';
 
-export default function Home({ userGraph = [], setUserGraph, mockData }) {
-  console.log(mockData)
-
+export default function EditPage({ userGraph={}, setUserGraph }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Social Graph - Home</title>
+        <title>Social Graph - Edit Data</title>
         <meta name="description" content="Social network graph to display contact groups. Edit the content displayed by the graph." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- pages/view.js
+++ pages/view.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
-import { useState, useEffect } from 'react';
-import styles from '../styles/View.module.css'
+import styles from '../styles/View.module.css';
 
-export default function Home({ userGraph = [], setUserGraph }) {
+export default function ViewPage({ userGraph={}, setUserGraph }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Social Graph - Home</title>
+        <title>Social Graph - View Graph</title>
         <meta name="description" content="Social network graph to display contact groups. View network graph here." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
